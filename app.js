782d8fda575ca4481804e04281ae2918c273fa84
Sheheryar Khan <81645279+Sheheryar09@users.noreply.github.com>
2023-05-16 21:56:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+var result = document.getElementById('result')
+
+function getNumber(num) {
+    result.value += num
+}
+
+function deleteValue() {
+    result.value = result.value.slice(0, result.value.length - 1);
+}
+
+function clearValue() {
+    result.value = ""
+}
+
+function getResult() {
+    if (result.value.length != 0 || result.value != "") {
+        var value = eval(result.value)
+        if (value == "undefined" || value == undefined) {
+            result.value = "Invalid expression"
+            return
+        }
+        result.value = value
+    }
+}
+
+function square() {
+    if (result.value.length != 0 || result.value != "") {
+        result.value = result.value * result.value
+    }
+}
+
+function percentage()
+{
+  result.value=(result.value/100);
+}
